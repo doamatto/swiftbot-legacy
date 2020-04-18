8c08ec5ea5a0ac5a2ccd47f2e9d68343cbbad3fa
doamatto <hello@doamatto.xyz>
2020-04-18 12:38:04 -0700

    Rewrote Config Script

--- frmwrk/config.go
+++ frmwrk/config.go
@@ -6,39 +6,18 @@ import (
 	"io/ioutil"
 )
 
-var (
-	token  string
-	prefix string
-
-	config *configStruct
-)
-
 type configStruct struct {
 	token  string `json:"token"`
 	prefix string `json:"prefix"`
 }
 
-func ReadConfig(filename string) error {
-	fmt.Println("Reading config file...")
-
-	file, err := ioutil.ReadFile(filename)
-
+func readConf(filename string) *configStruct {
+	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Println(err.Error())
-		return err
+		fmt.Println("error loading config,", err)
+		return nil
 	}
-
-	fmt.Println(string(file))
-
-	err = json.Unmarshal(file, &config)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	Token = config.Token
-	BotPrefix = config.BotPrefix
-
-	return nil
+	var conf configStruct
+	json.Unmarshal(body, &conf)
+	return &conf
 }
