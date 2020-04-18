b37a6a5156224bf8f1b37d608d4d730ff538b22e
doamatto <hello@doamatto.xyz>
2020-04-18 13:14:17 -0700

    Rewrite of main script, started frmwrk work

--- main.go
+++ main.go
@@ -3,19 +3,68 @@ package main
 import (
 	"fmt"
 
-	"./bot"
-	"./config"
+	"./frmwrk"
+	dgo "github.com/bwmarrin/discordgo"
 )
 
-func main() {
-	err := config.ReadConfig()
+var (
+	conf  *frmwrk.config
+	cmd   *frmwrk.CmdHandler
+	botID string
+	bot   *discordgo.Session
+)
+
+func init() {
+	conf = frmwrk.readConf("config.json") // Load Config file
+	prefix := conf.prefix // Set prefix
+}
+
+func start() {
+	cmd = frmwrk.cmdHandler() 
+	regCommands()
+	bot, err := discordgo.New(conf.token) // Start bot
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Couldn't start Discord bot: ", err)
 		return
 	}
 
-	bot.Start()
+	u, err := bot.User("@me") // Fetch account details
+	if err != nil {
+		fmt.Println("Couldn't fetch account details: ", err))
+		return
+	}
 
+	botID = u.ID
+	Bot.AddHandler(msgHandle) // Activate handler for messages
+
+	err = bot.Open()
+	if err != nil {
+		fmt.Println("Couldn't start bot:", err)
+		return
+	}
+	fmt.Println("The eagle has landed. Swift is running.")
 	<-make(chan struct{})
-	return
+}
+
+func msgHandle(s *dgo.Session, m *dgo.MessageCreate) {
+	content := m.Content
+	user := m.Author
+	if m.Author.ID == botID {
+		return
+	} // Prevent bot from messaging itself
+
+	if len(content) <= len(prefix) { return }
+	if content[:len(prefix)] != prefix { return }
+	content = content[len(prefix):]
+	if len(content) < 1 { return }
+
+	args := strings.Fields(content)
+	name := strings.ToLower(args[0])
+	cmd, found := cmdHandler.Get(name)
+
+	if !found { return }
+}
+
+func regCommands() {
+	cmdHandler.Reg("ping", cmd.ping, "First there's a ping, then there's a...")
 }
