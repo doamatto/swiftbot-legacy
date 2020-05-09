09c037128780bf21923e976ae50b11f5f9a767e4
doamatto <hello@doamatto.xyz>
2020-05-09 16:31:49 -0700

    Table flip and unflip added

---
+++ cmds/tableflip.js
@@ -0,0 +1,18 @@
+module.exports = {
+	run: function (djs, m, args) {
+		m.channel.send("(°-°)\\ ┬─┬").then(ms => {
+			setTimeout(() => {
+				ms.edit("(╯°□°)╯    ]").then(mss => {
+					setTimeout(() => {
+						mss.edit("(╯°□°)╯  ︵  ┻━┻");
+					}, 500);
+				});
+			}, 500);
+		});
+	},
+
+	help: {
+        name: "tableflip",
+        alias: "tf"
+	}
+};
