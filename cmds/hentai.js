d898403335b87fea9a9660eab968ed8196d0946d
doamatto <hello@doamatto.xyz>
2020-06-24 07:51:06 -0700

    Added hentai command/fuck command; closes #1

---
+++ cmds/hentai.js
@@ -0,0 +1,24 @@
+const nfetch = require("node-fetch");
+module.exports = {
+  run: function (djs, m, args) {
+    if(!m.channel.nsfw) {
+      return m.channel.send("To use this command, you need to run it in a channel marked as 'NSFW.'");
+    }
+    nfetch("https://nekos.life/api/v2/img/Random_hentai_gif", {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      m.channel.send(`Don't beat your meat too hard, mkay?`, {
+        embed: {
+          image: {
+            url: json.url
+          }
+        }
+      });
+    });
+  },
+  help: {
+      name: "hentai"
+  }
+};
