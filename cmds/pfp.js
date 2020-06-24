12fc21c4506fa8fbecd89eeee24f6c37c69a3af2
doamatto <hello@doamatto.xyz>
2020-06-24 11:37:16 -0700

    Fixed PFP command up

--- cmds/pfp.js
+++ cmds/pfp.js
@@ -1,14 +1,25 @@
 module.exports = {
     run: function (djs, m, args) {
-      let user = m.mentions.members.first();
-      if(user == undefined)
-        return m.channel.send("You need to tag the person you want to see the profile picture of");
-      m.channel.send({
-        embed: {
-          description: `${user.username} has a pretty dope profile picture. Check it out *[here](${ava})*`,
-          image: { url:user.displayAvatarURL }
-        }
+      let user = m.mentions.members.first() ? m.mentions.users.first() : m.author;
+      let ava = user.displayAvatarURL({
+        format: "png",
+        dynamic: true
       });
+      if(user === m.author) {
+        return m.channel.send({
+          embed: {
+            description: `You've got a pretty dope profile picture. Check it out *[here](${ava})*`,
+            thumbnail: { "url": ava }
+          }
+        });
+      } else {
+        return m.channel.send({
+          embed: {
+            description: `${user.username} has a pretty dope profile picture. Check it out *[here](${ava})*`,
+            thumbnail: { "url": ava }
+          }
+        });
+      }
     },
     help: {
         name: "pfp"
