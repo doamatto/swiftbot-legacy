55dc690dc7c1f88ab8c5402162cb1f7445965a01
doamatto <hello@doamatto.xyz>
2020-06-24 10:54:24 -0700

    Fixed command name for help

--- cmds/help.js
+++ cmds/help.js
@@ -3,6 +3,6 @@ module.exports = {
     return m.channel.send("Right now, I don't have a proper command list. You can see a list at https://github.com/doamatto/switbot/wiki/Commands, however");
   }, 
   help: {
-    name: "overwatch"
+    name: "help"
   }
 };
