a8437f4c07b1c4ffc62a7c8dfbcca92e2b5176b8
doamatto <hello@doamatto.xyz>
2020-06-25 22:33:06 -0700

    Condensed hentai command

--- cmds/hentai.js
+++ cmds/hentai.js
@@ -4,9 +4,8 @@ module.exports = {
     if(!m.channel.nsfw) {
       return m.channel.send("To use this command, you need to run it in a channel marked as 'NSFW.'");
     }
-    nfetch("https://nekos.life/api/v2/img/Random_hentai_gif", {
-      method: 'GET'
-    }).then(res => res.json()).then(json => {
+    nfetch("https://nekos.life/api/v2/img/Random_hentai_gif", { method: 'GET' })
+    .then(res => res.json()).then(json => {
       m.channel.send(`Don't beat your meat too hard, mkay?`, {
         embed: {
           image: { url: json.url }
