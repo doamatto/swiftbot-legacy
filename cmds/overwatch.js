7fca0f486aec6f9ec58de1c801a45c0f08844e58
doamatto <hello@doamatto.xyz>
2020-06-25 22:32:40 -0700

    Overhaul to error handling, fixed some bugs with region formatting, and made if statements easily collapsible using self-running funcs

--- cmds/overwatch.js
+++ cmds/overwatch.js
@@ -3,51 +3,58 @@ module.exports = {
   run: function (djs, m, args) {
     let region = args[0];
     let tag = args[1];
-    if(region == undefined || tag == undefined) {
-      return m.channel.send("Here's how you should run this command: `!overwatch {region} {tag}`. Your region is either `us`, `eu`, or `asia`. Your tag is something like `doamatto#1428`.");
-    }
+    region = region.toLowerCase();
+    if(region !== "us" || region !== "eu" || region !== "asia") {
+      return m.channel.send("That's not a valid region. Make sure the region is either 'us', 'eu', or 'asia'");
+    } // Ensures region is supported
     let username = tag.split("#")[0];
     if(username.length < 3 | username.length > 12) {
       return m.channel.send(`That doesn't look like a real username. They should be only 3-12 characters long. It seems your username is at ${username.length} characters.`);
-    }
+    } // Ensures username abides by Blizzard rules
     let tagAlt = tag.replace("#", "-"); // The API needs this to be a hypen instead
-    nfetch(`https://ow-api.com/v1/stats/pc/${region}/${tagAlt}/profile`, {
-      method: 'GET'
-    }).then(res => res.json()).then(json => {
+    nfetch(`https://ow-api.com/v1/stats/pc/${region}/${tagAlt}/profile`, { method: 'GET'})
+    .then(res => res.json()).then(json => {
       let qpStats = json.quickPlayStats;
       let compStats = json.competitiveStats;
       let level, prestige, rating;
-      if(json.prestige === 0 | json.prestige === undefined) {
-        level = `Level ${json.level}`;
-      } else {
-        prestige = json.prestige * 100; // Each prestige is 100 levels;
-        prestige += json.level; // Add the base level to the prestige level count
-        level = `Level ${prestige}`;
-      } // Calculate prestiges into levels
-      if(json.rating === 0) {
-        rating = "Not Ranked";
-      } else if(json.rating === 0 && json.level < 25){
-        rating = "Can't Play Comp";
-      } else {
-        if(rating >= 1 <= 1499) {
-          rating = `Ranked Bronze at ${json.rating}`;
-        } else if(rating >= 1500 <= 1999) {
-          rating = `Ranked Siler at ${json.rating}`;
-        } else if(rating >= 2000 <= 2999) {
-          rating = `Ranked Gold at ${json.rating}`;
-        } else if(rating >= 3000 <= 3499) {
-          rating = `Ranked Diamond at ${json.rating}`;
-        } else if(rating >= 3500 <= 3999) {
-          rating = `Ranked Master at ${json.rating}`;
-        } else if(rating >= 4000) {
-          rating = `Ranked Grandmaster at ${json.rating}`;
+      (function calcPrestiege() {
+        if(json.prestige === 0 | json.prestige === undefined) {
+          level = `Level ${json.level}`;
+        } else {
+          prestige = json.prestige * 100; // Each prestige is 100 levels;
+          prestige += json.level; // Add the base level to the prestige level count
+          level = `Level ${prestige}`;
         }
-      } // Calcualte comp rank based on rating
-      if(compStats.eliminationsAvg === undefined) {
-        comp = `${username} hasn't played competitive this season yet.`;
-      } else {
-        comp = `${username} has won ${compStats.games.won} games. They've got an average of ${compStats.eliminationsAvg} elims, an average of ${compStats.damageDoneAvg} hitpoints, and healed an average of ${compStats.healingDoneAvg} healing. ${compMedals}${username} has also earned ${compStats.awards.cards} cards.`;
-      } // Makes sure the user has played comp the current season
+      });// Calculate prestiges into levels
+      (function calcRank() {
+        if(json.rating === 0) {
+          rating = "Not Ranked";
+        } else if(json.rating === 0 && json.level < 25){
+          rating = "Can't Play Comp";
+        } else {
+          if(rating >= 1 <= 1499) {
+            rating = `Ranked Bronze at ${json.rating}`;
+          } else if(rating >= 1500 <= 1999) {
+            rating = `Ranked Siler at ${json.rating}`;
+          } else if(rating >= 2000 <= 2999) {
+            rating = `Ranked Gold at ${json.rating}`;
+          } else if(rating >= 3000 <= 3499) {
+            rating = `Ranked Diamond at ${json.rating}`;
+          } else if(rating >= 3500 <= 3999) {
+            rating = `Ranked Master at ${json.rating}`;
+          } else if(rating >= 4000) {
+            rating = `Ranked Grandmaster at ${json.rating}`;
+          }
+        }
+      }); // Calcualte comp rank based on rating
+      (function ensureComp() {
+        if(compStats.eliminationsAvg === undefined) {
+          comp = `${username} hasn't played competitive this season yet.`;
+        } else {
+          comp = `${username} has won ${compStats.games.won} games. They've got an average of ${compStats.eliminationsAvg} elims, an average of ${compStats.damageDoneAvg} hitpoints, and healed an average of ${compStats.healingDoneAvg} healing. ${compMedals}${username} has also earned ${compStats.awards.cards} cards.`;
+        }
+      }); // Makes sure the user has played comp the current season
+
       function medalCalc(type) {
         let awards;
         if(type === "comp") {
@@ -78,9 +85,7 @@ module.exports = {
         embed: {
           title: `${tag}'s profile`,
           url: `https://playoverwatch.com/en-us/career/pc/${tagAlt}`,
-          footer: {
-            "text": "The data from Ow-Api.com might not be completely accurate. You can click on the profile link to see the most accurate version of the data via PlayOverwatch.com"
-          },
+          footer: { "text": "The data from Ow-Api.com might not be completely accurate. You can click on the profile link to see the most accurate version of the data via PlayOverwatch.com" },
           thumbnail: { url: json.icon },
           author: {
             "name": rating,
