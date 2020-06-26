1960a064db8e3c6f081988da044b07e85cf2db4f
doamatto <hello@doamatto.xyz>
2020-06-25 22:33:21 -0700

    Condensed hug command and improved error handling

--- cmds/hug.js
+++ cmds/hug.js
@@ -1,35 +1,30 @@
 const nfetch = require("node-fetch");
 module.exports = {
     run: function (djs, m, args) {
-        if(m.mentions.members.size < 1)
-            return m.channel.send("Who did you say you were going to hug? (You need to mention the user you want to hug)");
-        if(m.mentions.members.size > 1)
-            return m.channel.send("You're such a great Care Bear. Unfortunately, Swift isn't. (Mention only one user to hug)");
-        let u = m.guild.member(m.mentions.members.first());
-        nfetch("https://nekos.life/api/hug", {
-            method: 'GET'
-        })
-        .then(res => res.json())
-        .then(json => {
-            if(m.mentions.members.first() === m.guild.me) {
-                m.channel.send(`Aww, thanks ${m.author.username}:)`, {
-                    embed: {
-                        image: {
-                            url: json.url
-                        }
-                    }
-                });
-            } else {
-                m.channel.send(`${m.author.username} hugged ${u}. How sweet.`, {
-                embed: {
-                    image: {
-                        url: json.url
-                    }
-                }
-            });
-
+      if(m.mentions.members.size < 1)
+        return m.channel.send("Who did you say you were going to hug? (You need to mention the user you want to hug)");
+      if(m.mentions.members.size > 1)
+        return m.channel.send("You're such a great Care Bear. Unfortunately, I'm not. (Mention only one user to hug)");
+      let u = m.guild.member(m.mentions.members.first());
+      nfetch("https://nekos.life/api/hug", { method: 'GET' })
+      .then(res => res.json()).then(json => {
+        if(m.mentions.members.first() === m.guild.me) {
+          m.channel.send(`Aww, thanks ${m.author.username}:)`, {
+            embed: {
+              image: { url: json.url }
             }
-        });
+          });
+        } else {
+          m.channel.send(`${m.author.username} hugged ${u}. How sweet.`, {
+            embed: {
+              image: { url: json.url }
+            }
+          });
+        }
+      }).on("error", (err) => {
+        console.error(`Fatal error when fetching a hug pic. Here's the error: ${err}`);
+        return m.channel.send(`Something went wrong. Go to https://github.com/doamatto/swiftbot/issues and report this bug. Make sure to mention the following: ${err}`)
+      });
     },
     help: {
         name: "hug"
