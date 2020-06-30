b0ea7a3ec785c2619ea5c27ab387af277d44a6d7
doamatto <hello@doamatto.xyz>
2020-06-30 08:46:28 -0700

    Added more aliases for commands

--- cmds/help.js
+++ cmds/help.js
@@ -3,6 +3,7 @@ module.exports = {
     return m.channel.send("Right now, I don't have a proper command list. You can see a list at https://github.com/doamatto/switbot/wiki/Commands, however");
   }, 
   help: {
-    name: "help"
+    name: "help",
+    aliases: [ "h", "cmds" ]
   }
 };
